e56442fb377afe7a3d4a3efc0ed650bc6952cb5b
saubhagya <saubhagya.carvia@gmail.com>
2024-03-17 22:25:06 +0530

    Design fixes

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from "@angular/material/card";
+import {MatButton} from "@angular/material/button";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatButton,
+    FormsModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  inputText: string = '';
 
+  onGenerate() {
+    //TODO: send the text to AI to create a latex template
+  }
 }
